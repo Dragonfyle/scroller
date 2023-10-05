200aa36c40fd9911f9ad28fb8b8473c5a22ba5af
Michal Rygorowicz <mitsugi23@hotmail.com>
2023-10-05 13:02:22 +0200

    add step scroll feature

--- js/main.js
+++ js/main.js
@@ -1,12 +1,41 @@
 document.addEventListener('DOMContentLoaded', function () {
-  console.log('loaded');
-
-  const rootElement = document.querySelector('#root');
+  //   const ROOT_ELEMENT = document.querySelector('#root');
   const sections = document.querySelectorAll('section');
+  let currentSectionIndex = 0;
+  let isThrottled = false;
+  const QUARTER_SECOND = 250;
 
-  console.log(sections, rootElement);
+  document.addEventListener(
+    'wheel',
+    function (e) {
+      e.preventDefault();
+      const direction = e.wheelDeltaY < 0 ? 1 : -1;
+      const NUMBER_OF_SECTIONS = sections.length - 1;
+      const FIRST_SECTION = 0;
+      const requestedSection = currentSectionIndex + direction;
+      console.log(e.wheelDelta);
 
-  document.addEventListener('mousewheel', function (e) {
-    console.log(e.wheelDelta);
-  });
+      if (
+        isThrottled ||
+        requestedSection > NUMBER_OF_SECTIONS ||
+        requestedSection < FIRST_SECTION
+      ) {
+        return;
+      }
+
+      isThrottled = true;
+
+      setTimeout(() => {
+        isThrottled = false;
+      }, QUARTER_SECOND);
+
+      currentSectionIndex = currentSectionIndex + direction;
+
+      sections[requestedSection].scrollIntoView(true, {
+        behavior: 'smooth',
+        block: 'start',
+      });
+    },
+    { passive: false }
+  );
 });
